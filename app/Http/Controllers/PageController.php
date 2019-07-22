d2153ad5364bdee97f165db01104fd29a1f805fe
James <jetigue@comcast.net>
2019-07-22 11:12:54 -0400

    Fixed announcements

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -16,7 +16,7 @@ class PageController extends Controller
      */
     public function welcome()
     {
-        $announcements = Announcement::orderBy('created_at')->get();
+        $announcements = Announcement::orderBy('created_at', 'asc')->get();
 
         $teamEvents = TeamEvent::orderBy('event_date', 'asc')->get();
 
