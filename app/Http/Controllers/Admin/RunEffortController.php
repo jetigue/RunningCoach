8aab524a60c8043d61ac2f5b60ad2c569e0df945
James <jetigue@comcast.net>
2019-07-21 20:17:55 -0400

    Update Admin routes

--- app/Http/Controllers/Admin/RunEffortController.php
+++ app/Http/Controllers/Admin/RunEffortController.php
@@ -8,6 +8,12 @@ use App\Http\Controllers\Controller;
 
 class RunEffortController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('admin');
+    }
+
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/Admin/TerrainTypeController.php
+++ app/Http/Controllers/Admin/TerrainTypeController.php
@@ -8,6 +8,11 @@ use App\Http\Controllers\Controller;
 
 class TerrainTypeController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('admin');
+    }
+
     /**
      * Display a listing of the resource.
      *
