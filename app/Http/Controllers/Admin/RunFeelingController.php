d706f9893b4366b71b9c4b7a97efe17c129e6d64
James <jetigue@comcast.net>
2019-07-23 10:39:48 -0400

    Changed Runninglog to RunningLog

--- app/Http/Controllers/Admin/RunFeelingController.php
+++ app/Http/Controllers/Admin/RunFeelingController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Models\Runninglog\RunFeeling;
+use App\Models\RunningLog\RunFeeling;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
