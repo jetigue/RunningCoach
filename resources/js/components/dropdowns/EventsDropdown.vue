38e97b55878edbcad77d9a01973b65dba948922e
James Tigue <jetigue@comcast.net>
2019-11-24 14:18:58 -0500

    Fixed dropdpown menu pictures

--- resources/js/components/dropdowns/EventsDropdown.vue
+++ resources/js/components/dropdowns/EventsDropdown.vue
@@ -12,16 +12,12 @@
                  class="absolute w-full z-10 bg-white h-64 max-h-100 border-b-2 border-gray-200">
 
                 <div class="flex flex-wrap h-full lg:w-3/4 mx-auto md:p-2">
-                    <div class="w-1/4"></div>
+                    <div class="our-events-menu flex w-1/4 h-full justify-center uppercase py-4">
+                    </div>
                     <div class="flex flex-col w-1/4 p-2">
                         <p class="text-xl text-primary font-semibold">Our Events</p>
                         <a class="pl-2" href="/longhorn-5k">Longhorn 5k</a>
                         <a class="pl-2" href="/team-camp">Team Camp</a>
-                    </div>
-                    <div class="flex flex-col w-1/4 p-2">
-
-                    </div>
-                    <div class="our-events-menu flex w-1/4 h-full justify-center uppercase py-4">
                     </div>
                 </div>
 
--- resources/js/components/dropdowns/ResultsDropdown.vue
+++ resources/js/components/dropdowns/ResultsDropdown.vue
@@ -13,18 +13,17 @@
 
 
                 <div class="flex flex-wrap h-full lg:w-3/4 mx-auto md:p-2">
-                    <div class="w-1/4"></div>
-                    <div class="flex flex-col w-1/4 p-2">
+                    <div class="our-results-menu flex w-1/3 h-full justify-center uppercase py-4">
+                    </div>
+                    <div class="flex flex-col w-1/3 p-2">
                         <p class="text-xl text-primary font-semibold">Cross Country</p>
                         <a class="pl-2" href="/cross-country-meets">Meet Results</a>
                         <a class="pl-2" href="/cross-country-venues">Venue Records</a>
                     </div>
-                    <div class="flex flex-col w-1/4 p-2">
+                    <div class="flex flex-col w-1/3 p-2">
                         <p class="text-xl text-primary font-semibold">Track</p>
                         <a class="pl-2" href="#">Meet Results (coming soon)</a>
                         <a class="pl-2" href="#">School Records (coming soon)</a>
-                    </div>
-                    <div class="our-results-menu flex w-1/4 h-full justify-center uppercase py-4">
                     </div>
                 </div>
 
