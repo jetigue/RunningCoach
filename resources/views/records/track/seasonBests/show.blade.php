8db8be70559f2839f6856c5040911e22b35e6686
James Tigue <jetigue@comcast.net>
2020-03-16 20:34:58 -0400

    fix 3200m url

--- resources/views/records/track/seasonBests/show.blade.php
+++ resources/views/records/track/seasonBests/show.blade.php
@@ -7,7 +7,7 @@
             <div class="flex lg:flex-col w-full lg:w-1/6 flex-wrap justify-around font-semibold lg:justify-start">
                 <p class="px-2 py-1 text-blue-700"><a href="/track/season-bests/athletes-season-bests/800m">800m Run</a></p>
                 <p class="px-2 py-1 text-blue-700"><a href="/track/season-bests/athletes-season-bests/1600m">1600m Run</a></p>
-                <p class="px-2 py-1 text-blue-700"><a href="/track/season-bests/athletes-season-bests/1200m">3200m Run</a></p>
+                <p class="px-2 py-1 text-blue-700"><a href="/track/season-bests/athletes-season-bests/3200m">3200m Run</a></p>
                 <p class="px-2 py-1 text-blue-700"><a href="/track/season-bests/athletes-season-bests/1200m">1200m Run</a></p>
                 <p class="px-2 py-1 text-blue-700"><a href="/track/season-bests/athletes-season-bests/1500m">1500m Run</a></p>
             </div>
