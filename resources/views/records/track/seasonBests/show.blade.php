2fd08541ddded6e6f9ff958e980c6d367a88e0f7
James Tigue <jetigue@comcast.net>
2020-03-16 20:29:49 -0400

    Items Start Event Menu

--- resources/views/records/track/seasonBests/show.blade.php
+++ resources/views/records/track/seasonBests/show.blade.php
@@ -4,7 +4,7 @@
     <div class="flex flex-col w-full">
         <header class="text-3xl lg:text-5xl text-primary font-thin text-center p-2">Athletes' Season Bests</header>
         <div class="flex flex-wrap justify-around w-full">
-            <div class="flex lg:flex-col w-full lg:w-1/6 flex-wrap justify-around font-semibold lg:content-start">
+            <div class="flex lg:flex-col w-full lg:w-1/6 flex-wrap justify-around font-semibold lg:items-start">
                 <p class="px-2 py-1 text-blue-700"><a href="/track/season-bests/athletes-season-bests/800m">800m Run</a></p>
                 <p class="px-2 py-1 text-blue-700"><a href="/track/season-bests/athletes-season-bests/1600m">1600m Run</a></p>
                 <p class="px-2 py-1 text-blue-700"><a href="/track/season-bests/athletes-season-bests/1200m">3200m Run</a></p>
