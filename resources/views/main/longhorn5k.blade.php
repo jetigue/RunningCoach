d237bd413399ac8e7339bc61afdc096423b27fe6
James Tigue <jetigue@comcast.net>
2021-10-28 09:08:15 -0400

    Add Course Map Link for 5k

--- resources/views/main/longhorn5k.blade.php
+++ resources/views/main/longhorn5k.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
 
-<div class="flex flex-col w-full mx-auto min-h-screen p-4 lg:px-8">
+<div class="flex flex-col w-full min-h-screen p-4 mx-auto lg:px-8">
 
-    <p class="text-primary text-3xl font-thin text-left">Longhorn 5k</p>
-    <div class="primary-line my-1"></div>
+    <p class="text-3xl font-thin text-left text-primary">Longhorn 5k</p>
+    <div class="my-1 primary-line"></div>
 
-    <div class="flex flex-wrap py-2 justify-between items-center">
+    <div class="flex flex-wrap items-center justify-between py-2">
         <div class="flex w-full lg:w-7/12">
             <div class="flex flex-col leading-normal">
-                <a href="https://runsignup.com/Race/GA/Cumming/Longhorn5K" type="button" class="py-2 px-2 bg-crimson-600 text-white rounded-md text-center font-semibold text-lg mb-4">Click to Register!</a>
+                <a href="https://runsignup.com/Race/GA/Cumming/Longhorn5K" type="button" class="px-2 py-2 mb-4 text-lg font-semibold text-center text-white rounded-md bg-crimson-600">Click to Register!</a>
                 <p class="py-1">
                    Join us to Run with the Horns on Sunday, November 14, 2021.  This is the 13th annual 5K with proceeds benefiting the Longhorn Cross Country team!  This year we have changed our name, changed our venue, and upped our game as we will be running a certified USATF Course so that you can use our race as your Official AJC Peachtree Road Race Qualifying Event.
                 </p>
@@ -23,20 +23,23 @@
                 <p class="py-1">
                     For more information, please e-mail Stacey Mumford at french_mumford@hotmail.com. Thanks in advance for supporting Lambert Cross Country!
                 </p>
+                <a class="py-1 font-semibold"
+                    href="https://drive.google.com/file/d/1j5lA2xQITyNVkvOBSaXf_jgpMglBr3QT/view?usp=sharing">Course Map</a>
 
             </div>
         </div>
 
-        <div class="flex w-full md:py-2 lg:w-1/3 justify-center">
-            <div class="flex flex-col bg-white rounded shadow p-2 lg:p-4">
+        <div class="flex justify-center w-full md:py-2 lg:w-1/3">
+            <div class="flex flex-col p-2 bg-white rounded shadow lg:p-4">
                 <figure>
                     <img src="{{url('images/longhorn_5k/flyer2021.PNG')}}" width="100%" alt="Longhorn 5k">
                 </figure>
             </div>
         </div>
     </div>
-    <div class="flex flex-col w-full lg:w-1/2 py-2">
-        <p class="text-primary text-xl text-left">Past Results</p>
+
+    <div class="flex flex-col w-full py-2 lg:w-1/2">
+        <p class="text-xl text-left text-primary">Past Results</p>
         <a class="text-blue-600 hover:underline"
            href="https://www.trisignup.com/Race/Results/47756/?resultSetId=213837&textVersion">
             2020 Longhorn 5k Results
